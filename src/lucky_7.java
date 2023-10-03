e87ff1ba5c88e57dfffc0c3b8d6cc6663da72f9d
Sofku800 <142890732+Sofku800@users.noreply.github.com>
2023-10-03 13:47:46 +0300

    Vaihe 2
    Lisäsin peliin rahaa
    yksi peli kierros -1€.
    Voitot antavat rahaa, seiskojen määrän mukaan.
    Jos menettää kaikki rahat, peli loppuu.

--- src/lucky_7.java
+++ src/lucky_7.java
@@ -10,7 +10,11 @@ public class lucky_7 {
 		
 
 		Random random = new Random();
+		int money;
+		money = 5;
 		
+		do {
+			
 		int randomNumber1;
 		int randomNumber2;
 		int randomNumber3;
@@ -19,41 +23,78 @@ public class lucky_7 {
 		randomNumber2 = random.nextInt(10);
 		randomNumber3 = random.nextInt(10);
 		
+		
+		
 		Scanner in = new Scanner(System.in);
 		String vastaus;
 		
 		
-		do {
+		
 		System.out.println("Haluatko pelata Lucky7 peliä? (kyllä tai ei) ");
+		System.out.println("Rahaa " + money + "€");
+		
 		vastaus = in.nextLine();
-			
+		
+		
+		if (vastaus.equals("ei"))
+		{
+			break;
+		}
+		
 		
 		
 		if (vastaus.equals("kyllä")) 
 		{ 
+			money=(money-1);
+			
+			System.out.println();
 			System.out.print(randomNumber1);
 			System.out.print(randomNumber2);
 			System.out.print(randomNumber3);
+			System.out.println();
 		}
 		
-		if(vastaus.equals("ei")) 
-		{
-			break;
-		}
+		
 		
 		if (randomNumber1 == 7 || randomNumber2 == 7 || randomNumber3 == 7 ) 
 		{
 			System.out.println();
 			System.out.println("Onneksi olkoon sinä voitit!");
 		}
+		
+		
+		
+		if (randomNumber1 == 7 && randomNumber2 == 7 && randomNumber3 == 7 ) 
+		{
+//			System.out.println("Rahaa" + (money+7) + "€");
+			money = (money+7);
+		}
+		if (randomNumber1 == 7 && randomNumber2 == 7 || randomNumber2 == 7 && randomNumber3 == 7) 
+		{
+			money = (money+5);
+		}
+		if (randomNumber1 == 7 || randomNumber2 == 7 || randomNumber3 == 7 ) 
+		{
+			money = (money+3);
+		}
+		
+		
 		else 
 		{
 			System.out.println();
 			System.out.println("Hävisit");
 		}
+		if(money <= 0) 
+		{
+			System.out.println("Rahat loppuivat");
+			break;
+		}
+		
+		System.out.println(money);
 		
 		}
 		while (true);
+		
 		
 		
 		
