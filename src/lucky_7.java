3019e2c2c8871191b2b25a6d7f9d0509d67e15ea
Sofku800 <142890732+Sofku800@users.noreply.github.com>
2023-10-04 10:14:35 +0300

    Vaihe 3
    Nyt pelaaja voi lisätä rahamäärän.
    Enterillä voi pelata uudestaan, "e" poistut pelistä.

--- src/lucky_7.java
+++ src/lucky_7.java
@@ -10,14 +10,29 @@ public class lucky_7 {
 		
 
 		Random random = new Random();
-		int money;
-		money = 5;
 		
-		do {
-			
+		Scanner in = new Scanner(System.in);
+		
+		String vastaus;
+		int money;
 		int randomNumber1;
 		int randomNumber2;
 		int randomNumber3;
+		
+		
+//		System.out.println("Haluatko pelata Lucky7 peliä? (kyllä tai ei) ");
+//		vastaus = in.nextLine();
+		
+		System.out.println("Syötä rahamäärä, jolla haluat pelata");
+		String vastausRaha = in.nextLine();
+		
+		
+        money = Integer.parseInt(vastausRaha);
+            
+		System.out.println("Rahaa " + money + "€");
+		
+		do {
+			
 		
 		randomNumber1 = random.nextInt(10);
 		randomNumber2 = random.nextInt(10);
@@ -25,26 +40,14 @@ public class lucky_7 {
 		
 		
 		
-		Scanner in = new Scanner(System.in);
-		String vastaus;
+		
+//		if (vastaus.equals("ei"))
+//		{
+//			break;
+//		}
 		
 		
-		
-		System.out.println("Haluatko pelata Lucky7 peliä? (kyllä tai ei) ");
-		System.out.println("Rahaa " + money + "€");
-		
-		vastaus = in.nextLine();
-		
-		
-		if (vastaus.equals("ei"))
-		{
-			break;
-		}
-		
-		
-		
-		if (vastaus.equals("kyllä")) 
-		{ 
+
 			money=(money-1);
 			
 			System.out.println();
@@ -52,7 +55,6 @@ public class lucky_7 {
 			System.out.print(randomNumber2);
 			System.out.print(randomNumber3);
 			System.out.println();
-		}
 		
 		
 		
@@ -60,47 +62,57 @@ public class lucky_7 {
 		{
 			System.out.println();
 			System.out.println("Onneksi olkoon sinä voitit!");
-		}
+		} 
+		
+		
 		
 		
 		
 		if (randomNumber1 == 7 && randomNumber2 == 7 && randomNumber3 == 7 ) 
 		{
-//			System.out.println("Rahaa" + (money+7) + "€");
+
 			money = (money+7);
 		}
-		if (randomNumber1 == 7 && randomNumber2 == 7 || randomNumber2 == 7 && randomNumber3 == 7) 
+		else if (randomNumber1 == 7 && randomNumber2 == 7 || randomNumber2 == 7 && randomNumber3 == 7 || randomNumber1 ==7 && randomNumber3 == 7) 
 		{
 			money = (money+5);
 		}
-		if (randomNumber1 == 7 || randomNumber2 == 7 || randomNumber3 == 7 ) 
+		else if (randomNumber1 == 7 || randomNumber2 == 7 || randomNumber3 == 7 ) 
 		{
 			money = (money+3);
 		}
-		
 		
 		else 
 		{
 			System.out.println();
 			System.out.println("Hävisit");
 		}
-		if(money <= 0) 
+		System.out.println("Rahaa: " + money + "€");
+		
+		System.out.println("Haluatko pelata uudestaan? Jos haluat pelata, paina Enter. Jos et halua pelata, kirjoita e ja paina Enter");
+		vastaus = in.nextLine();
+//		
+		
+		if(vastaus.equals("e"))
+		{
+		 break;
+		}
+
+		if(money <= 0)
 		{
 			System.out.println("Rahat loppuivat");
 			break;
 		}
 		
-		System.out.println(money);
+		
+		
+		
 		
 		}
 		while (true);
 		
 		
-		
-		
-		
-		
-		
+		System.out.println("Kiitos pelaamisesta");
 
 		}
 	
